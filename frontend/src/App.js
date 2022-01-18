e96a3575f5f9c8819dce1766dee20b17e0fa7594
Jimmy Baranello <jimmy.baranello@gmail.com>
2022-01-17 22:04:02 -0500

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <Login></Login>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/register" element={<Register/>} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
