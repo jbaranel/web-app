6e71a1f1ee7e5c5efabbb99dc6bb4e740ea2d3c4
Jimmy Baranello <jimmy.baranello@gmail.com>
2022-01-26 19:35:34 -0500

    db connector

--- server/api/helpers/db_connector.js
+++ server/api/helpers/db_connector.js
@@ -1,34 +1,16 @@
 import knex from "knex"
+import config from "../../config.js"
 
-export const database = knex({
+export async function connection () {
+  const database =  knex({
     client: 'mysql2',
     connection: {
-      host : process.env.DB_URI,
+      host : config.DB_URI,
       port : 3306,
-      user : process.env.DB_USERNAME,
-      password : process.env.DB_PASSWORD,
-      database : process.env.DB_NAME
+      user : config.DB_USERNAME,
+      password : config.DB_PASSWORD,
+      database : config.DB_NAME
     }
   });
-
-
-const f = async () => {
-    const user = {
-        user_id: "2c70498b-9fe1l4d17-a992-5e32649cda79",
-        username: "123p4",
-        password: "$2b$10$G/heihdXmKlKBXTKheyYsuq02fOlHQq8RSa/aCX6jNDWJWLS5uOaa",
-        lastName: "Baranello",
-        email:"Taraklb@optonline.net",
-        firstName:  "Tara"
-        }
-    const res = await database('User').insert(user)
-    console.log(res)
-    database.destroy()
+  return database
 }
-
-const g = async () => {
-    const res = await database.select().from("User").where({ username: '1234' })
-    console.log(res)
-    database.destroy()
-}
-
